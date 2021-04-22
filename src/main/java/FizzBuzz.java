0c03917795909d8b5ebce976a3403047bad7aa21
Andrew2a1 <adam.brz@onet.eu>
2021-04-22 15:51:19 +0200

    Creation of fizzbuzz

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,5 +1,23 @@
+import java.util.ArrayList;
+
 public class FizzBuzz {
     public String generateTo(int max) {
-        return "1";
+        ArrayList<String> outFizzBuzz = new ArrayList<>();
+
+        for(int i = 1; i <= max; ++i)
+            outFizzBuzz.add(getFizzBuzzForNumber(i));
+
+        return String.join(" ", outFizzBuzz);
+    }
+
+    private String getFizzBuzzForNumber(int number) {
+        String out = "";
+
+        if(number % 3 == 0)
+            out += "Fizz";
+        if(number % 5 == 0)
+            out += "Buzz";
+
+        return !out.isEmpty() ? out : String.valueOf(number);
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -12,6 +12,25 @@ class FizzBuzzTest {
     @Test
     void FizzBuzzForTwoIs12() {
         FizzBuzz fizzbuzz = new FizzBuzz();
-        assertEquals("1 2", fizzbuzz.generateTo(1));
+        assertEquals("1 2", fizzbuzz.generateTo(2));
+    }
+
+    @Test
+    void FizzBuzzForThreeIs12Fizz() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertEquals("1 2 Fizz", fizzbuzz.generateTo(3));
+    }
+
+    @Test
+    void FizzBuzzForFiveIs12Fizz4Buzz() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertEquals("1 2 Fizz 4 Buzz", fizzbuzz.generateTo(5));
+    }
+
+    @Test
+    void FizzBuzzForFifteenHasFizzBuzz() {
+        FizzBuzz fizzbuzz = new FizzBuzz();
+        assertEquals("1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz",
+                fizzbuzz.generateTo(15));
     }
 }
